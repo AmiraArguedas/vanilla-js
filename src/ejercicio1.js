a149b7e25126987b1d98dcaf53635c03156e9306
unknown <aarguedas@fwdcostarica.com>
2024-08-14 09:33:41 -0600

    ejercicio terminado / rama 2

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,8 +1,21 @@
+
+
 let contador = {
   valor: 0,
+
   siguiente: function () {
-    // Inserte el código aquí
-  },
-};
+    
+    return this.valor++
+
+    
+  }
+}
+
+
+
+
+
+
 
 export { contador }
+
--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,7 +1,23 @@
+
+
 class Contador {
   // Inserte el código aquí
+  valor = 0
+  
+  siguiente(){
+    return this.valor++
+  }
+
+
 }
 
 // Inserte el código aquí
+let instancia = new Contador()
+
+
+
+
+
+
 
 export { instancia }
